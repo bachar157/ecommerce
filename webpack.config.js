21811fee0ea45ce7c84a9f28690631aabc5b6af7
bacharalhamada <bachar.alhmada75@gmail.com>
2021-04-28 19:00:42 +0300

    commit message

---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  
+  entry: {
+    app: "./src/index.js",
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: '',
+    filename: "main.js",
+  },
+
+  mode: "development",
+
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    port: 1233,
+    overlay: true,//for errors
+  writeToDisk: true,
+  open:true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    
+      {
+  test: /\.(sa|sc|c)ss$/,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader, 
+        options: {
+          publicPath: '../' ,
+          //minimize: true, قمنا بحذف هذا السطر
+        }
+      },
+      'css-loader',
+      'sass-loader'
+    ]
+},
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "images",
+            }
+          }
+        ]
+      },
+
+     
+    {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+          use: [
+            {
+              loader: "file-loader", 
+              options: {
+                name: '[name].[ext]',
+                outputPath: "fonts",
+                esModule: false,
+              }
+            }
+          ]
+      },
+      
+  
+      {
+        test: require.resolve("jquery"),
+        loader: 'expose-loader',
+        options:{
+          exposes:['$','jquery'],
+        }
+      }
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "product.html",
+      template: "./src/product.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "checkout.html",
+      template: "./src/checkout.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "payment.html",
+      template: "./src/payment.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "search.html",
+      template: "./src/search.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "./src/contact.html",
+    }),
+
+
+
+    new MiniCssExtractPlugin({ filename: "css/style.css" }),
+  ],
+
+
+};
